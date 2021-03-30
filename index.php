8faa6b05a4d6681b0c27845ce629ae69b1257662
Ishanveersg <ishanveersg@outlook.com>
2021-03-30 17:27:30 -0400

    Updated project

--- index.php
+++ index.php
@@ -2,37 +2,74 @@
 <html>
 <head>
     <title>Athletes Leaderboard</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="includes/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="includes/css/jquery-ui.css">
+    <script type="text/javascript" src="includes/js/bootstrap.js"></script>
+    <script type="text/javascript" src="includes/js/jquery-ui.js"></script>
 </head>
-<body>
-<table>
-    <tr>
-        <th>id</th>
-        <th>identifier</th>
-        <th>name</th>
-        <th>created</th>
-        <th>modified</th>
-        <th>dob</th>
-        <th>Identified Gender</th>
-    </tr>
-    <?php
-    require_once 'includes/php/postgreSQL_connect.php';
-    /**
-     * @var string $conn
-     */
-    $query = "SELECT * FROM athletes";
-    $result = pg_query($conn, $query);
-    while($row= pg_fetch_assoc($result)){
-        echo "<tr><td>" .
-            $row["id"]. "</td><td>" .
-            $row["identifier"] . "</td><td>" .
-            $row["name"] . "</td><td>" .
-            $row["created"] . "</td><td>" .
-            $row["modified"] . "</td><td>" .
-            $row["dob"] . "</td><td>" .
-            $row["identified_gender"]. "</td></tr>";
-    }
-    ?>
-</table>
+<body style="font-family: sans-serif">
+<div class="container">
+    <div class="row mt-3">
+        <form action="index1.php" method="POST">
+            <div class="form-group w-100">
+                    <input type="text" class="form-control" name="name" placeholder="Filter by name" aria-label="...">
+            </div>
+            <div class="form-group w-100">
+                <input class="form-control" name="date" placeholder="Filter by date " aria-label="...">
+            </div>
+            <div class="form-group w-100">
+                <input type="submit" name="submit" class="btn-dark" >
+            </div>
+        </form>
+    </div>
+</div>
+<div class="row container-fluid w-75 m-auto">
+    <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th>Name</th>
+                <th>D.O.B</th>
+                <th>Identified Gender</th>
+                <th>Nationality</th>
+            </tr>
+        </thead>
+        <?php
+        require_once 'includes/php/postgreSQL_connect.php';
+        /**
+         * @var string $conn
+         */
+        function displayTable($res){
+            while ($row = pg_fetch_assoc($res)) {
+                echo "<tr><td>" .
+                    //$row["id"] . "</td><td>" .
+                    //$row["identifier"] . "</td><td>" .
+                    $row["name"] . "</td><td>" .
+                    $row["dob"] . "</td><td>" .
+                    $row["identified_gender"] . "</td><td>" .
+                    $row["nationality"] . "</td></tr>";
+            }
+        }
+        if(isset($_POST['submit'])){
+            //$id = $_POST['id'];
+            //$identifier = $_POST['identifier'];
+            $name = $_POST['name'];
+            //$dob = $_POST['dob'];
+            //$identified_gender = $_POST['identified_gender'];
+
+            if($name != "") {
+                $query = "SELECT * FROM athletes WHERE name LIKE'%$name%'";
+                $result = pg_query($conn, $query);
+                displayTable($result);
+            }
+            else {
+                $query = "SELECT * FROM athletes";
+                $result = pg_query($conn, $query);
+                displayTable($result);
+            }
+        }
+        ?>
+    </table>
+</div>
 </body>
 </html>
